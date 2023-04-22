5427fb6bb941b5f9a6776918cc69431fa9b29236
u7469758 <u7469758@anu.edu.au>
2023-04-22 15:18:00 +1000

    AVLTree insertion implemented and informally tested

--- MyEducationalApp/lib/src/main/java/com/example/lib/AVLTree.java
+++ MyEducationalApp/lib/src/main/java/com/example/lib/AVLTree.java
@@ -1,5 +1,7 @@
 package com.example.lib;
 
+import java.util.ArrayList;
+
 public class AVLTree <T extends Comparable<T>> {
     // Keep a track of how many past questions have been deleted (e.g. wrong questions have been posted so need to be removed?)
     // delete() will be the last one to implement if we have time
@@ -7,58 +9,165 @@ public class AVLTree <T extends Comparable<T>> {
     public Node<T> root;
     public AVLTree() {this.root = null;}
 
-    private void rightRotate(){}
+    /**
+     * Performs the right rotation at a given node
+     * @param root is a root of a subtree, not necessarily the root of the whole tree
+     */
+    private void rightRotate(Node<T> root) {
+        //Temporary reference to the left child of the current root of the subtree
+        Node<T> newRoot = root.left;
+        root.left = newRoot.right;
 
-    private void leftRotate(){}
+        //Updates the parents of the nodes
+        if (newRoot.right != null) {
+            newRoot.right.parent = root;
+        }
+        newRoot.parent = root.parent;
 
-    private void insertBeforeBalance(Node<T> root, Node<T> x) {
-        if (root.value.compareTo(x.value) > 0) {
-            if (root.left.value == null) {
-                root.left = x;
-                x.parent = root;
-            } else {
-                insertBeforeBalance(root.left, x);
+        //Check whether the root is actual root of the whole tree (which should never happen in our specification),
+        //then update this newRoot as the left or right child of the original root's parent
+        if (root.parent == null) {
+            this.root = newRoot;
+        } else if (root == root.parent.right) {
+            root.parent.right = newRoot;
+        } else {
+            root.parent.left = newRoot;
+        }
+        newRoot.right = root;
+        root.parent = newRoot;
+    }
+
+    private void leftRotate(Node<T> root) {
+        Node<T> newRoot = root.right;
+        root.right = newRoot.left;
+
+        //Updates the parents of the nodes
+        if (newRoot.left != null) {
+            newRoot.left.parent = root;
+        }
+        newRoot.parent = root.parent;
+
+        //Check whether the root is actual root of the whole tree (which should never happen in our specification),
+        //then update this newRoot as the left or right child of the original root's parent
+        if (root.parent == null) {
+            this.root = newRoot;
+        } else if (root == root.parent.right) {
+            root.parent.right = newRoot;
+        } else {
+            root.parent.left = newRoot;
+        }
+        newRoot.left = root;
+        root.parent = newRoot;
+    }
+
+    private int getBalance(Node<T> node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int leftHeight = (node.left != null) ? node.left.getHeight() : 0;
+        int rightHeight = (node.right != null) ? node.right.getHeight() : 0;
+
+        return leftHeight - rightHeight;
+    }
+
+    private Node<T> findImbalance(Node<T> leaf){
+        if (root == null) {
+//            System.out.println("null");
+            return null;
+        }
+
+        if (Math.abs(getBalance(leaf)) > 1){
+//            System.out.println(leaf.value);
+            return leaf;
+        } else if (leaf.parent != null){
+            return findImbalance(leaf.parent);
+        }
+//        System.out.println("null!");
+        return null;
+    }
+
+    private void balanceSubtree(Node<T> node) {
+        int balanceFactor = getBalance(node);
+        if (balanceFactor > 1) {
+            if (getBalance(node.left) <= -1) {
+                leftRotate(node.left);
             }
-        } else if (root.value.compareTo(x.value) < 0) {
-            if (root.right.value == null) {
-                root.right = x;
-                x.parent = root;
+            rightRotate(node);
+        } else if (balanceFactor < -1) {
+            if (getBalance(node.right) >= 1) {
+                rightRotate(node.right);
+            }
+            leftRotate(node);
+        }
+    }
+
+    private void insertBeforeBalance(Node<T> root, Node<T> curr) {
+        if (root.value.compareTo(curr.value) > 0) {
+            if (root.left.value == null) {
+                root.left = curr;
+                curr.parent = root;
             } else {
-                insertBeforeBalance(root.right, x);
+                insertBeforeBalance(root.left, curr);
+            }
+        } else if (root.value.compareTo(curr.value) < 0) {
+            if (root.right.value == null) {
+                root.right = curr;
+                curr.parent = root;
+            } else {
+                insertBeforeBalance(root.right, curr);
             }
         }
         // Do nothing if the tree already has a node with the same value.
     }
 
-    private void insert(Node<T> x) {
+    private void insert(Node<T> newNode) {
         if (this.root == null) {
-            this.root = x;
+            this.root = newNode;
         } else {
-            insertBeforeBalance(this.root, x);
+            insertBeforeBalance(this.root, newNode);
+            Node<T> rootOfImbalance = findImbalance(newNode);
+            if (rootOfImbalance != null) {
+                balanceSubtree(rootOfImbalance);
+            }
         }
     }
 
     public void insert(T value) {
-        Node<T> node = new Node<T>(value);
+        Node<T> node = new Node<>(value);
         insert(node);
     }
 
-    private Node<T> find(Node<T> x, T v) {
-        if (x.value == null) {
+    public Node<T> find(Node<T> node, T v) {
+        if (node.value == null) {
             return null;
         }
 
-        int cmp = v.compareTo(x.value);
+        int cmp = v.compareTo(node.value);
         if (cmp < 0)
-            return find(x.left, v);
+            return find(node.left, v);
         else if (cmp > 0)
-            return find(x.right, v);
+            return find(node.right, v);
         else
-            return x;
+            return node;
     }
 
-    public Node<T> search(T key) {
-        return find(this.root, key);
+    public Node<T> search(T v) {
+        return find(this.root, v);
+    }
+
+    public ArrayList<Node<T>> preOrderTraversal(){
+        ArrayList<Node<T>> nodes = new ArrayList<>();
+        preOrderHelper(this.root, nodes);
+        return nodes;
+    }
+
+    private void preOrderHelper(Node<T> curr, ArrayList<Node<T>> nodes) {
+        if (curr != null) {
+            nodes.add(curr);
+            preOrderHelper(curr.left, nodes);
+            preOrderHelper(curr.right, nodes);
+        }
     }
 
     @Override
@@ -76,8 +185,8 @@ public class AVLTree <T extends Comparable<T>> {
             sb.append("\n");
 
             String paddingForBoth = padding + "|  ";
-            String pointerRight = "R---";
-            String pointerLeft = "L---";
+            String pointerRight = "R___";
+            String pointerLeft = "L___";
 
             toStringHelper(sb, paddingForBoth, pointerLeft, node.left);
             toStringHelper(sb, paddingForBoth, pointerRight, node.right);
@@ -87,23 +196,19 @@ public class AVLTree <T extends Comparable<T>> {
     public static void main(String[] args) {
         //Hard coded test to quickly check its implementation
         AVLTree<Integer> tree = new AVLTree<>();
-        Integer[] nodes = new Integer[]{4,3,2,5,6,-1,10,9,7};
-        for (Integer node: nodes){
-            tree.insert(node);
+        Integer[] nodesValues = new Integer[]{4,2,3,0,5,6,10,9,7};
+        for (Integer value : nodesValues) {
+            tree.insert(value);
         }
-        System.out.println(tree.root.value);
-        System.out.println(tree.root.left.value);
-        System.out.println(tree.root.left.left.value);
-        System.out.println(tree.root.right.value);
-        System.out.println(tree.root.left.getHeight());
-        System.out.println(tree.root.getBalanceFactor());
-        System.out.println(tree.root.right.getBalanceFactor());
-        System.out.println(tree.root.left.getBalanceFactor());
-
         System.out.println(tree);
+
+        ArrayList<Node<Integer>> nodes = tree.preOrderTraversal();
+        for (Node<Integer> node : nodes) {
+            System.out.println("Value: " + node.value + ", BF: " +  tree.getBalance(node));
+        }
     }
 
-    public class Node<E extends Comparable<E>> {
+    public static class Node<E extends Comparable<E>> {
 
         E value; // Node value - hash value of past questions?
         Node<E> parent, left, right; // this parent, this left and right nodes
@@ -113,8 +218,8 @@ public class AVLTree <T extends Comparable<T>> {
             this.parent = null;
 
             // Initialise children leaf nodes
-            this.left = new Node<E>();
-            this.right = new Node<E>();
+            this.left = new Node<>();
+            this.right = new Node<>();
             this.left.parent = this;
             this.right.parent = this;
         }
@@ -131,8 +236,9 @@ public class AVLTree <T extends Comparable<T>> {
             return Math.max(leftNodeHeight, rightNodeHeight);
         }
 
-        public int getBalanceFactor() {
-            return left.getHeight() - right.getHeight();
-        }
+//        public int getBalanceFactor() {
+//            return left.getHeight() - right.getHeight();
+//        }
     }
 }
+
