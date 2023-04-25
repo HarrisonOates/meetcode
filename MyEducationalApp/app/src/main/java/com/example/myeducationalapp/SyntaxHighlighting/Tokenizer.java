fe6a9e6098ff336320265987db4777885bea0451
Harrison Oates <u7468212@anu.edu.au>
2023-04-25 10:09:48 +1000

    Created base Token class for syntax highlighting

---
+++ MyEducationalApp/app/src/main/java/com/example/myeducationalapp/SyntaxHighlighting/Tokenizer.java
@@ -0,0 +1,13 @@
+package com.example.myeducationalapp.SyntaxHighlighting;
+
+/**
+ * Tokenizer class to convert a file of java code into tokens
+ * At present, tokenizer does not check for correctness.
+ * @author Harrison Oates
+ */
+public class Tokenizer {
+    // Placeholder taken from Lab 5
+    private String buffer;
+    private Token currentToken;
+
+}
