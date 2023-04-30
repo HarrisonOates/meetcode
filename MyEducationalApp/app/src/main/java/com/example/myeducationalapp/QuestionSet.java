400bd8a27830c916eab90ecdcd53481a216e0763
u7469758 <u7469758@anu.edu.au>
2023-04-30 22:12:08 +1000

    how do we want to structure our QuestionSet?

--- MyEducationalApp/app/src/main/java/com/example/myeducationalapp/QuestionSet.java
+++ MyEducationalApp/app/src/main/java/com/example/myeducationalapp/QuestionSet.java
@@ -2,7 +2,10 @@ package com.example.myeducationalapp;
 
 import com.example.myeducationalapp.Search.SearchToken;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
@@ -10,30 +13,53 @@ public class QuestionSet {
     public enum Category {Algorithm, ControlFlow, DataStructure, Miscellaneous, Recursion}
 
     // <Question ID, String[]{Question, answer} pair
-    public HashMap<String, String[]> questionSets;
+    public HashMap<String, String[]> unusedQuestionSets;
+    public HashMap<String, String[]> usedQuestionSets;
+    public final char[] categoryRotation = new char[]{'0', '1', '2', '3', '4'};
+    public int categoryIndex = 0;
     public HashMap<Category, Character> categoryCharacter;
     public QuestionSet() {
-        questionSets = new HashMap<>();
+        unusedQuestionSets = new HashMap<>();
+        usedQuestionSets = new HashMap<>();
         categoryCharacter = new HashMap<>();
         categoryCharacter.put(Category.Algorithm, '0');
         categoryCharacter.put(Category.ControlFlow, '1');
         categoryCharacter.put(Category.DataStructure, '2');
         categoryCharacter.put(Category.Miscellaneous, '3');
         categoryCharacter.put(Category.Recursion, '4');}
-    public Set<String> getQuestionIDs(){
-        return questionSets.keySet();
+    public Set<String> futureQuestionIDs(){
+        return unusedQuestionSets.keySet();
     }
-
+    public Set<String> pastQuestionIDs(){
+        return usedQuestionSets.keySet();
+    }
     // Type 0: Algo, 1: ControlFlow, 2: DataStructure, 3: Misc, 4: Recursion | difficulty: 1 ~ 5
     public void addQuestion(String question, String answer, Category category, int difficulty) {
         String uniqueID;
         // Make sure each ID is indeed unique
         do {
             uniqueID = categoryCharacter.get(category) + difficulty + UUID.randomUUID().toString();
-        } while (questionSets.containsKey(uniqueID));
-        questionSets.put(uniqueID, new String[]{question, answer});
+        } while (unusedQuestionSets.containsKey(uniqueID));
+        unusedQuestionSets.put(uniqueID, new String[]{question, answer});
     }
 
+    /**
+     * Randomly gets the question of the day where the category rotates every day.
+     */
+    public String[] getQuestionOfTheDay() {
+        categoryIndex = (categoryIndex + 1) % 5;
+        String uniqueID;
+        do {
+            List<String> keys = new ArrayList<>(unusedQuestionSets.keySet());
+            Collections.shuffle(keys);
+            uniqueID = keys.get(0);
+        } while (uniqueID.charAt(1) != categoryRotation[categoryIndex]);
+        String[] question = unusedQuestionSets.get(uniqueID);
+        unusedQuestionSets.remove(uniqueID);
+        usedQuestionSets.put(uniqueID, question);
+        return question;
+
+    }
     // I think we can just call one of these questions once a day?
     public void dataStructure1() {
         addQuestion(
@@ -49,6 +75,7 @@ public class QuestionSet {
         );
     }
 
+    // TODO: add some questions and is this the structure of QuestionSet we want to use?
     public void dataStructure2() {
         // addQuestion();
     }
