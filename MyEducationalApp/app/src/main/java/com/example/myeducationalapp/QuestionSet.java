8894cae83c507914e8d4bbade03364a43633c0ed
u7469758 <u7469758@anu.edu.au>
2023-04-30 23:33:37 +1000

    DataStructure questions added

--- MyEducationalApp/app/src/main/java/com/example/myeducationalapp/QuestionSet.java
+++ MyEducationalApp/app/src/main/java/com/example/myeducationalapp/QuestionSet.java
@@ -63,33 +63,106 @@ public class QuestionSet {
     }
     // I think we can just call one of these questions once a day?
     public void dataStructure1() {
+        // Question about HashMap
         addQuestion(
-                "What will be the output of map.get(2) after the execution of the codes below? \n"+
-                        "A) 1, B) 2, C) 5, D) -1, E) 6 \n\n"+
-                        "Map<Integer, String> map = new HashMap<Integer, String>(); \n"+
-                        "map.put(1, \"2\"); \n"+
-                        "map.put(2, \"5\"); \n"+
-                        "map.put(2, \"-1\"); \n"+
-                        "map.put(-1, \"6\");",
-                "D",
-                Category.DataStructure, "1"
-        );
+                "What will be the output of map.get(2) after the execution of the codes below?\n\n"+
+                        "Map<Integer, String> map = new HashMap<Integer, String>();\n"+
+                        "map.put(1, \"2\");\n"+
+                        "map.put(2, \"5\");\n"+
+                        "map.put(2, \"-1\");\n"+
+                        "map.put(-1, \"6\");\n\n" +
+                        "A) 1, B) 2, C) 5, D) -1, E) 6",
+                "D", Category.DataStructure, "1");
     }
 
     // TODO: add some questions and is this the structure of QuestionSet we want to use?
     public void dataStructure2() {
-        // addQuestion();
+        // Question about LinkedList
+        addQuestion(
+                "What is the time complexity of the `reverse()` method in the `LinkedList`?\n\n" +
+                        "public void reverse() {\n" +
+                        "        Node<T> prev = null;\n" +
+                        "        Node<T> current = head;\n" +
+                        "        while (current != null) {\n" +
+                        "            Node<T> next = current.next;\n" +
+                        "            current.next = prev;\n" +
+                        "            prev = current;\n" +
+                        "            current = next;\n" +
+                        "        }\n" +
+                        "        head = prev;\n" +
+                        "    }\n\n" +
+                        "A) O(1), B) O(n), C) O(n^2), D) O(log n), E) O(n log n)",
+                "B", QuestionSet.Category.DataStructure, "2");
     }
-
     public void dataStructure3() {
-        // addQuestion();
+        // Question about BinaryTree
+         addQuestion(
+                 "What will be the output of the following code?\n\n" +
+                         "BinaryTree tree = new BinaryTree(1);\n" +
+                         "tree.left = new BinaryTree(2);\n" +
+                         "tree.right = new BinaryTree(3);\n" +
+                         "tree.left.left = new BinaryTree(4);\n" +
+                         "tree.left.right = new BinaryTree(5);\n" +
+                         "tree.right.left = new BinaryTree(6);\n" +
+                         "tree.right.right = new BinaryTree(7);\n\n" +
+                         "List<Integer> result = tree.inOrderTraversal();\n" +
+                         "System.out.println(result);\n\n" +
+                         "A) [1, 2, 4, 5, 3, 6, 7]\n" +
+                         "B) [4, 2, 5, 1, 6, 3, 7]\n" +
+                         "C) [4, 5, 2, 6, 7, 3, 1]\n" +
+                         "D) [1, 2, 3, 4, 5, 6, 7]\n" +
+                         "E) [7, 6, 3, 5, 4, 2, 1]",
+                 "A", QuestionSet.Category.DataStructure, "3");
     }
 
     public void dataStructure4() {
-        // addQuestion();
+        // Question about stack
+        addQuestion(
+                "What is the result of weirdCalculator(\"234*+5+\")?\n\n " +
+                        "public static int weirdCalculator(String expression) {\n" +
+                        "   Stack<Integer> stack = new Stack<>();\n" +
+                        "   for (int i = 0; i < expression.length(); i++) {\n" +
+                        "       char c = expression.charAt(i);\n" +
+                        "       if (Character.isDigit(c)) {\n" +
+                        "           stack.push(c - '0');\n" +
+                        "       } else {\n" +
+                        "           int operand2 = stack.pop();\n" +
+                        "           int operand1 = stack.pop();\n" +
+                        "           int result = 0;\n" +
+                        "           switch (c) {\n" +
+                        "               case '+':\n" +
+                        "                   result = operand1 + operand2;\n" +
+                        "                   break;\n" +
+                        "               case '-':\n" +
+                        "                   result = operand1 - operand2;\n" +
+                        "                   break;\n" +
+                        "               case '*':\n" +
+                        "                   result = operand1 * operand2;\n" +
+                        "                   break;\n" +
+                        "               case '/':\n" +
+                        "                   result = operand1 / operand2;\n" +
+                        "                   break;\n" +
+                        "           }\n" +
+                        "           stack.push(result);\n" +
+                        "       }\n" +
+                        "   }\n" +
+                        "   return stack.pop();\n" +
+                        "}\n\n" +
+                        "A) 10, B) 11, C) 12, D) 15, E) 19",
+                "E", QuestionSet.Category.DataStructure, "4");
     }
 
     public void dataStructure5() {
-        // addQuestion();
+        // Question about BST
+        addQuestion(
+                "Given an integer n, what is the number (G(n)) of structurally unique BST's " +
+                        "which has exactly n nodes of unique values from 1 to n?\n" +
+                        "Initial State: G(0) = 1 G(1) = 1\n\n" +
+                        "A) G(n) = SUM(G(i - 1) * G(n - i)) over i = 1, ..., n - 1\n" +
+                        "B) G(n) = SUM(G(i - 1) * G(n - i)) over i = 1, ..., n\n" +
+                        "C) G(n) = SUM(G(i) * G(n - i)) over i = 1, ..., n\n" +
+                        "D) G(n) = SUM(G(i - 1) * G(n - 1)) over i = 1, ..., n - 1\n" +
+                        "E) G(n) = SUM(G(i) * G(n - i)) over i = 1, ..., n - 1",
+                "B" ,QuestionSet.Category.DataStructure, "5");
     }
 }
