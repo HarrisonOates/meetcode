b7d27f6b22be73e73aac826fc8616d682f53d356
Harrison Oates <48871176+HarrisonOates@users.noreply.github.com>
2023-05-14 13:27:30 +1000

    Added outline for how to implement the data instances requirement

---
+++ MyEducationalApp/app/src/main/java/com/example/myeducationalapp/DatastreamSimulation/DataGenerator.java
@@ -0,0 +1,47 @@
+package com.example.myeducationalapp.DatastreamSimulation;
+
+import com.example.myeducationalapp.DirectMessageThread;
+
+import java.util.Random;
+
+/**
+ * Simulates a data stream to satisfy the basic features of the app.
+ * @author Harrison
+ */
+public class DataGenerator {
+
+
+    /**
+     * Generates the data for the app to read from.
+     */
+    public static void generateData(){
+        // TODO - remove after implementation
+
+        // This is how to send direct messages to firebase.
+        /*
+        DirectMessageThread dms = new DirectMessageThread("username");
+        dms.runWhenReady((ignored) -> {
+            dms.postMessage("hi");
+            return null;
+        });
+         */
+        // use new QuestionMessageThread instead for comments on questions.
+
+        // This is how to like messages
+        /*
+        DirectMessageThread dms = new DirectMessageThread("username");
+        dms.runWhenReady((ignored) -> {
+           dms.getMessages().get(message_index).runWhenReady((ignored2) -> {
+               dms.getMessages().get(message_index).toggleLikedByCurrentUser();
+               return null;
+           });
+        });
+         */
+
+        Random r = new Random();
+
+        // TODO - set up the files to read from and iterate at least 2500 times.
+        // TODO -  implement three second delay between each loop.
+
+    }
+}
