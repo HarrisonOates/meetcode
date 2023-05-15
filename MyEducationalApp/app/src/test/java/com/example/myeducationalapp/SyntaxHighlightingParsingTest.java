a365267bf6d11fa300bdd8629090821fa22586ef
Harrison Oates <48871176+HarrisonOates@users.noreply.github.com>
2023-05-15 10:05:20 +1000

    Updated tests to reflect the changes made for < and > to render in html properly

--- MyEducationalApp/app/src/test/java/com/example/myeducationalapp/SyntaxHighlightingParsingTest.java
+++ MyEducationalApp/app/src/test/java/com/example/myeducationalapp/SyntaxHighlightingParsingTest.java
@@ -17,14 +17,7 @@ public class SyntaxHighlightingParsingTest {
                         "```for(int i = 0; i < 5; i++){\n" +
                         "    sum += 1;\n" +
                         "}```";
-        String expected = "Hello world! I know what I want to do. <br>Let's write code! <br>" +
-                "<font color = \"orange\">int</font>&nbsp;<font color = \"purple\">i</font>&nbsp;<font color = \"purple\">=</font>&nbsp;<font color = \"blue\">0</font><font color = \"purple\">;</font>"
-                + "<br>Here is some more code <br>"
-                + "<font color = \"orange\">for</font><font color = \"purple\">(</font><font color = \"orange\">int</font>"
-                + "&nbsp;<font color = \"purple\">i</font>&nbsp;<font color = \"purple\">=</font>&nbsp;<font color = \"blue\">0</font><font color = \"purple\">;"
-                + "</font>&nbsp;i&nbsp;<font color = \"purple\"><</font>&nbsp;<font color = \"blue\">5</font><font color = \"purple\">;</font>&nbsp;i<font color = \"purple\">+</font><font color = \"purple\">+</font><font color = \"purple\">)</font><font color = \"purple\">{</font>"
-                + "<br>&nbsp;&nbsp;&nbsp;&nbsp;<font color = \"purple\">sum</font>&nbsp;<font color = \"purple\">+</font><font color = \"purple\">=</font>&nbsp;<font color = \"blue\">1</font><font color = \"purple\">;</font>"
-                + "<br><font color = \"purple\">}</font>";
+        String expected = "Hello world! I know what I want to do. <br>Let's write code! <br><font color = \"orange\">int</font>&nbsp;<font color = \"purple\">i</font>&nbsp;<font color = \"purple\">=</font>&nbsp;<font color = \"blue\">0</font><font color = \"purple\">;</font><br>Here is some more code <br><font color = \"orange\">for</font><font color = \"purple\">(</font><font color = \"orange\">int</font>&nbsp;<font color = \"purple\">i</font>&nbsp;<font color = \"purple\">=</font>&nbsp;<font color = \"blue\">0</font><font color = \"purple\">;</font>&nbsp;i&nbsp;<font color = \"purple\">&lt;</font>&nbsp;<font color = \"blue\">5</font><font color = \"purple\">;</font>&nbsp;i<font color = \"purple\">+</font><font color = \"purple\">+</font><font color = \"purple\">)</font><font color = \"purple\">{</font><br>&nbsp;&nbsp;&nbsp;&nbsp;<font color = \"purple\">sum</font>&nbsp;<font color = \"purple\">+</font><font color = \"purple\">=</font>&nbsp;<font color = \"blue\">1</font><font color = \"purple\">;</font><br><font color = \"purple\">}</font>";
 
         String found = DetectCodeBlock.parseCodeBlocks(s);
         System.out.println(found);
