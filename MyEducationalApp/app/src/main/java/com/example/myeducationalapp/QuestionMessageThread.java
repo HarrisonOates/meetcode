159778baddbe09cfae016833c30ca0dd3d0e164d
Alex Boxall <alexboxall3@gmail.com>
2023-05-16 19:37:23 +1000

    Added null check to QuestionMessageThread

--- MyEducationalApp/app/src/main/java/com/example/myeducationalapp/QuestionMessageThread.java
+++ MyEducationalApp/app/src/main/java/com/example/myeducationalapp/QuestionMessageThread.java
@@ -3,9 +3,6 @@ package com.example.myeducationalapp;
 import com.example.myeducationalapp.Firebase.Firebase;
 import com.example.myeducationalapp.Firebase.FirebaseResult;
 
-import java.util.Arrays;
-import java.util.List;
-
 /**
  * Contains all of the messages posted under a given question.
  *
@@ -23,7 +20,11 @@ public class QuestionMessageThread extends MessageThread {
     @Override
     FirebaseResult downloadMessages() {
         return Firebase.getInstance().readQuestionComments(questionID).then((obj) -> {
-            List<String> allStrings = Arrays.asList(((String) obj).split("\n"));
+            if (obj == null) {
+                return null;
+            }
+
+            String[] allStrings = ((String) obj).split("\n");
             threadID = getThreadID();
             int index = 0;
             for (String message: allStrings) {
