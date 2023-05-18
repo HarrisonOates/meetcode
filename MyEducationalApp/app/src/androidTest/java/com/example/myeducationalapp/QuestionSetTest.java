cb2c9c9d53cb00ef1f0b10975301e241c4761577
Alex Boxall <alexboxall3@gmail.com>
2023-05-18 20:30:18 +1000

    Revived, moved and fixed QuestionSetTest.java

---
+++ MyEducationalApp/app/src/androidTest/java/com/example/myeducationalapp/QuestionSetTest.java
@@ -0,0 +1,24 @@
+package com.example.myeducationalapp;
+
+import static org.junit.Assert.*;
+
+import com.example.myeducationalapp.Question.QuestionSet;
+
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * @author u7468248 Alex Boxall
+ */
+
+public class QuestionSetTest {
+    @Test
+    public void testQuestionsInMiscCategory() {
+        QuestionSet qs = QuestionSet.getInstance();
+
+        List<String> questions = qs.getQuestionIDsInCategory(QuestionSet.Category.Miscellaneous);
+        assertEquals(questions.size(), 5);
+        assertEquals(qs.getUsedQuestionSets().get(questions.get(1)).getAnswer(), "028064");
+    }
+}
